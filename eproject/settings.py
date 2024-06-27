2d5cea100d4c45fed23c5077293f67be8687a129
Arun Babu <arunbabu43916@gmail.com>
2024-06-27 13:30:47 +0530

    Update settings.py

--- eproject/settings.py
+++ eproject/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'arun.23pmc110@mariancollege.org'
-EMAIL_HOST_PASSWORD = '23pmc110'
+# EMAIL_HOST_USER = 'your email'
+# EMAIL_HOST_PASSWORD = 'password'
